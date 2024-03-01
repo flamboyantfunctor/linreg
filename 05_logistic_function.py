378976f24bc361bca73c272347e55d79588a1178
Alkan Tasdan <alkantasdan@gmail.com>
2024-03-01 09:29:54 +0100

    added interactivity to the logistic function

--- 05_logistic_function.py
+++ 05_logistic_function.py
@@ -1,11 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button
 
-x = np.arange(-5, 5, 0.01)
-w = 10.0
-b = 1.0
+# Define the inputs and starting parameters
+x = np.arange(-10, 10, 0.1)
+W_MIN = 0.0
+W_MAX = 20.0
+W_INIT = 10.0
+
+B_MIN = -10.0
+B_MAX = 10.0
+B_INIT = 0.0
 
 
+# Define the logistic function
 def logistic_function(x: np.ndarray, w: float, b: float):
 
     m = x.shape[0]
@@ -19,10 +27,46 @@ def logistic_function(x: np.ndarray, w: float, b: float):
     return y
 
 
-y = logistic_function(x, w, b)
+# Compute the y values
+y = logistic_function(x, W_INIT, B_INIT)
 
-plt.plot(x, y, c="hotpink", label="logistic")
-plt.title("Logistic Function")
-plt.ylabel("y")
-plt.xlabel("x")
+fig, ax = plt.subplots()
+fig.subplots_adjust(left=0.25, bottom=0.25)
+(line,) = ax.plot(x, y)
+
+
+ax_w = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+w_slider = Slider(ax=ax_w, label="w", valmin=W_MIN, valmax=W_MAX, valinit=W_INIT)
+
+ax_b = fig.add_axes([0.1, 0.25, 0.0225, 0.63])
+b_slider = Slider(
+    ax=ax_b,
+    label="b",
+    valmin=B_MIN,
+    valmax=B_MAX,
+    valinit=B_INIT,
+    orientation="vertical",
+)
+
+
+def update(val):
+    line.set_ydata(logistic_function(x, w_slider.val, b_slider.val))
+    fig.canvas.draw_idle()
+
+
+def reset(event):
+    w_slider.reset()
+    b_slider.reset()
+
+
+# Connect the update function to sliders change events
+w_slider.on_changed(update)
+b_slider.on_changed(update)
+
+# Create and place a reset button
+resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+button = Button(resetax, "Reset", hovercolor="hotpink")
+
+button.on_clicked(reset)
+
 plt.show()
