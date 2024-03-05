27bb5d8db0528945567e001368272960ee1cfc8b
Alkan Tasdan <alkantasdan@gmail.com>
2024-03-05 08:46:47 +0100

    rearranged stuff and explanations

--- 07_logistic_loss.py
+++ 07_logistic_loss.py
@@ -4,15 +4,14 @@ import matplotlib.pyplot as plt
 # Inputs: Training Examples with 2 features
 X = np.array([[0.5, 1.5], [1, 1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
 
-# Corresponding labels for classification
+# Ouputs: Corresponding input labels
 y = np.array([0, 0, 0, 1, 1, 1]).reshape(-1, 1)
-# ...reshape(-1, 1) -> [[0],[0],[0],[1],[1],[1]
+# ...reshape(-1, 1) -> [[0],[0],[0],[1],[1],[1]]
 
 # I just love list comprehensions.. Shoutout to Haskell!
 ones = [X[i] for i in range(len(X)) if y[i] == 1]
 zeros = [X[i] for i in range(len(X)) if y[i] == 0]
 
-fig, ax = plt.subplots(1, 1, figsize=(6, 6))
 
 """
 Decision Boundary
@@ -40,12 +39,6 @@ x_0_m1 = -x_1_m1 + 3
 x_1_m2 = np.arange(10)
 x_0_m2 = -x_1_m2 + 4
 
-# This *-moves are f-ing elegant 😳 i don't get it yet...
-"""
-*ones                   -> unpacks *ones, so [3, 0.5], [2, 2], [1, 2.5]
-zip(*ones)              -> zip([3, 0.5], [2, 2], [1, 2.5]) => [3, 2, 1], [0.5, 2, 2.5]
-(*zip(*ones))           -> *zip.. unpacks the two iters that zip(*ones) returns
-"""
 
 """
 Cost function:
@@ -54,7 +47,7 @@ Cost function:
 
 def compute_logistic_cost(X, y, w, b):
     """
-    Computes cost
+    Computes tht total cost for the logistic regression
 
     Args:
       X (ndarray (m,n)): Data, m examples with n features
@@ -65,7 +58,7 @@ def compute_logistic_cost(X, y, w, b):
     Returns:
       cost (scalar): cost
     """
-    m = X.shape[0]
+    m, _ = X.shape
 
     cost = 0
     for i in range(m):
@@ -77,24 +70,30 @@ def compute_logistic_cost(X, y, w, b):
     return cost
 
 
-text_m1 = f"Loss of model 1: {compute_logistic_cost(X, y, w_m1, b_m1)}"
-text_m2 = f"Loss of model 2: {compute_logistic_cost(X, y, w_m2, b_m2)}"
+text_m1 = f"Cost of model 1: {compute_logistic_cost(X, y, w_m1, b_m1)}"
+text_m2 = f"Cost of model 2: {compute_logistic_cost(X, y, w_m2, b_m2)}"
 
+fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+
+# Elegant zipping and unpacking.. i don't get it yet
+"""
+*ones                   -> unpacks *ones, so [3, 0.5], [2, 2], [1, 2.5]
+zip(*ones)              -> zip([3, 0.5], [2, 2], [1, 2.5]) => [3, 2, 1], [0.5, 2, 2.5]
+(*zip(*ones))           -> *zip.. unpacks the two iters that zip(*ones) returns
+"""
 # Plotting the training examples with label y=1
 ax.scatter(*zip(*ones), c="r", marker="x", label="y=1")
 # Plotting the training examples with label y=1
 ax.scatter(*zip(*zeros), c="b", marker="o", label="y=0")
 # Plotting the decision boundary
-ax.plot(x_0_m1, x_1_m1, "#7C8363", linestyle="dashed", label="b=-3")
-ax.plot(x_0_m2, x_1_m2, "#31473A", linestyle="dashed", label="b=-4")
+ax.plot(x_0_m1, x_1_m1, c="y", linestyle="dashed", label="model 1")
+ax.plot(x_0_m2, x_1_m2, c="g", linestyle="dashed", label="model 2")
 # Configuring the figure
 ax.axis([0.0, 4.0, 0.0, 4.0])
 ax.set_xlabel("$x_0$")
 ax.set_ylabel("$x_1$")
 plt.subplots_adjust(left=0.15, bottom=0.15)
-plt.text(3, 2.75, text_m1, color="#7C8363", ha="center")
-plt.text(3, 2.5, text_m2, color="#31473A", ha="center")
+plt.text(3, 2.75, text_m1, color="y", ha="center")
+plt.text(3, 2.5, text_m2, color="g", ha="center")
 plt.legend()
-
-# Show plot
 plt.show()
